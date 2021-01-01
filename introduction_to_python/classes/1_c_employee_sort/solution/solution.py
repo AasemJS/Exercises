ee407b97515c11d44df9e6492f7e555e1c68befa
Aasem <aasem.javed@gmail.com>
2021-01-02 05:21:58 +0530

    pushed on 2nd Jan

---
+++ introduction_to_python/classes/1_c_employee_sort/solution/solution.py
@@ -0,0 +1,13 @@
+from operator import attrgetter, itemgetter
+
+class Employee:
+    def __init__(self, name, employee_id, salary, years_at_company):
+        self.name = name
+        self.employee_id = employee_id
+        self.salary = salary
+        self.years_at_company = years_at_company
+
+
+def sort_employees(employee_list):
+    sorted(employee_list, key=itemgetter('name'))
+
